4f6bfbde3fb3a73222c8ae66589a4ff384b95114
karupoiss00 <glizerin1108@gmail.com>
2023-10-01 14:45:37 +0300

    Починил сборку

--- apps/leards/components/screens/notFound/NotFound.tsx
+++ apps/leards/components/screens/notFound/NotFound.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import BaseLayout from '../../common/BaseLayout'
+import styles from './NotFound.module.css'
 
 function get404ImageSrc() {
 	const isHorribleBackground = Math.round(Math.random() * 100) % 2 === 0
@@ -9,9 +9,9 @@ function get404ImageSrc() {
 
 function NotFound() {
 	return (
-		<BaseLayout>
+		<div className={styles.layout}>
 			<Image src={get404ImageSrc()} alt={'background'} layout={'fill'} draggable={false}/>
-		</BaseLayout>
+		</div>
 	)
 }
 
