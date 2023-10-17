9cb88e94b7730db560adbb88387b7bbebdad8fb1
Barabashhka <pavlik.danilov.2003@mail.ru>
2023-10-17 14:13:19 +0300

    Поправил градиент

--- apps/uikit-demo/src/app/stories/avatar/AvatarStory.tsx
+++ apps/uikit-demo/src/app/stories/avatar/AvatarStory.tsx
@@ -1,13 +1,25 @@
 import {Avatar} from '@viewshka/uikit'
 import ComponentStory, {StoryColumn} from '../../common/ComponentStory'
 import {addStory} from '../../stories'
+import styles from './Avatar.module.css'
 
 
 function AvatarStory() {
 	return (
 		<ComponentStory>
 			<StoryColumn>
-				<Avatar username={'qwerty'} size={'large'} />
+				<div className={styles['size-container']}>
+					<Avatar username={'Артемий Арсибеков'} size={'large'}/>
+					<Avatar username={'Ростислав Глизерин'} size={'large'}/>
+					<Avatar username={'Даниил Худяков'} size={'large'}/>
+					<Avatar username={'leards'} size={'large'}/>
+				</div>
+				<div className={styles['size-container']}>
+					<Avatar username={'Артемий Арсибеков'} size={'small'}/>
+					<Avatar username={'Ростислав Глизерин'} size={'small'}/>
+					<Avatar username={'Даниил Худяков'} size={'small'}/>
+					<Avatar username={'leards'} size={'small'}/>
+				</div>
 			</StoryColumn>
 		</ComponentStory>
 	)
--- libs/uikit/src/lib/avatar/Avatar.tsx
+++ libs/uikit/src/lib/avatar/Avatar.tsx
@@ -4,17 +4,17 @@ import styles from './Avatar.module.css'
 
 type AvatarSize = 'large' | 'small'
 
-type AvatarProps = PropsWithClassname & {
+type AvatarProps = {
     username: string
     avatarUrl?: string
     size: AvatarSize
 }
 
-function Avatar({username, avatarUrl, size, className}: AvatarProps) {
+function Avatar({username, avatarUrl, size}: AvatarProps) {
 	if (avatarUrl !== null && avatarUrl !== undefined) {
 		return (
 			<div className={classnames(styles['avatar-container'], {
-				[styles[`avatar-container--${size}`]]: true, className,
+				[styles[`avatar-container--${size}`]]: true,
 			})}>
 			</div>
 		)
@@ -22,37 +22,51 @@ function Avatar({username, avatarUrl, size, className}: AvatarProps) {
 	else {
 		return (
 			<div className={classnames(styles['avatar-container'], {
-				[styles[`avatar-container--${size}`]]: true, className,
+				[styles[`avatar-container--${size}`]]: true,
 			})}>
-				<div style={{
-					background: generateRandomGradient(),
-					width: '100%',
-					height: '100%',
-				}}
+				<div className={classnames(styles['avatar-gradient'])}
+					style={{
+						background: generateRandomGradient(username),
+					}}
 				>
+					<p className={styles['avatar-text']}>
+						{size == 'large' ? getAvatarName(username) : null}
+					</p>
 				</div>
 			</div>
 		)
 	}
 }
 
-function generateRandomGradient() {
-	const angle = Math.floor(Math.random() * 361)
-	const color1 = generateRandomColor()
-	const color2 = generateRandomColor()
+function generateRandomGradient(username: string) {
+	const angle = 180
+	const color1 = generateRandomColor(username)
+	const color2 = generateRandomColor(username.length < 4 ? username + username : username.substring(0, 4))
 
 	return `linear-gradient(${angle}deg, ${color1}, ${color2})`
 }
 
-function generateRandomColor() {
-	const letters = '0123456789ABCDEF'
+function generateRandomColor(username: string) {
+	let hash = 0
+	username.split('').forEach(char => {
+		hash = char.charCodeAt(0) + ((hash << 5) - hash)
+	})
 	let color = '#'
-
-	for (let i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)]
+	for (let i = 0; i < 3; i++) {
+		const value = (hash >> (i * 8)) & 0xff
+		color += value.toString(16).padStart(2, '0')
 	}
-
 	return color
+}
+
+function getAvatarName(username: string): string {
+	const strs: Array<string> = username.split(' ')
+	if (strs.length > 1) {
+		return strs[0][0] + strs[1][0]
+	}
+	else {
+		return strs[0].substring(0, 2)
+	}
 }
 
 export {
