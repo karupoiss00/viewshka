dde223c786e61ef1e17a48dfdd24cce562cde736
Barabashhka <pavlik.danilov.2003@mail.ru>
2023-11-30 23:11:37 +0300

    Сделал изменение аватарки

--- apps/leards/components/common/viewmodel/userAtom.ts
+++ apps/leards/components/common/viewmodel/userAtom.ts
@@ -26,8 +26,21 @@ const set = action((ctx, {user}: SetUserPayload) => {
 	})
 })
 
+const setAvatarUrl = action((ctx, avatarUrl: string) => {
+	const user = ctx.get(userAtom)
+	userAtom(ctx, {
+		...user,
+		avatarUrl,
+	})
+
+	ctx.schedule(() => {
+		UserProvider.setAvatarUrl(avatarUrl)
+	})
+})
+
 const userActions = {
 	set,
+	setAvatarUrl,
 } as const
 
 export {
