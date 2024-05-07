045abb0e243546ba44b536dc2ab7335fd86cf131
Rostislav Glizerin <glizerin1108@gmail.com>
2024-05-08 00:15:58 +0300

    Порефакторил код

--- apps/leards/next.config.js
+++ apps/leards/next.config.js
@@ -1,42 +1,41 @@
-//@ts-check
+/* eslint-disable */
+// @ts-nocheck
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const {withNx} = require('@nrwl/next/plugins/with-nx')
+
+function allowCamelCaseClassNames(config) {
+	const rules = config.module.rules
+		.find(rule => typeof rule.oneOf === 'object')
+		.oneOf.filter(rule => Array.isArray(rule.use))
+
+	rules.forEach(rule => {
+		rule.use.forEach(moduleLoader => {
+			const isCssLoader = moduleLoader.loader !== undefined
+				&& moduleLoader.loader.includes('css-loader')
+				&& typeof moduleLoader.options.modules === 'object'
+			if (isCssLoader) {
+				moduleLoader.options = {
+					...moduleLoader.options,
+					modules: {
+						...moduleLoader.options.modules,
+						exportLocalsConvention: 'camelCase',
+					},
+				}
+			}
+		})
+	})
+
+	return config
+}
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
 	nx: {
-		// Set this to true if you would like to to use SVGR
-		// See: https://github.com/gregberge/svgr
 		svgr: false,
 	},
-	webpack: config => {
-		const rules = config.module.rules
-			.find(rule => typeof rule.oneOf === 'object')
-			.oneOf.filter(rule => Array.isArray(rule.use))
-		rules.forEach(rule => {
-			rule.use.forEach(moduleLoader => {
-				if (
-					moduleLoader.loader !== undefined
-            && moduleLoader.loader.includes('css-loader')
-            && typeof moduleLoader.options.modules === 'object'
-				) {
-					moduleLoader.options = {
-						...moduleLoader.options,
-						modules: {
-							...moduleLoader.options.modules,
-							// This is where we allow camelCase class names
-							exportLocalsConvention: 'camelCase',
-						},
-					}
-				}
-			})
-		})
-
-		return config
-	},
+	webpack: allowCamelCaseClassNames,
 }
 
 module.exports = withNx(nextConfig)
