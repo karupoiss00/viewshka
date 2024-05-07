5700db5ed4e7a0c185a0453b377c31f5bf0a0240
Rostislav Glizerin <glizerin1108@gmail.com>
2024-05-08 00:14:44 +0300

    Revert "Немного почистил код"
    This reverts commit c407f25a15404125b58e6ac9fa3ca81bba7588ea.

--- apps/leards/next.config.js
+++ apps/leards/next.config.js
@@ -1,41 +1,42 @@
-/* eslint-disable */
-// @ts-nocheck
+//@ts-check
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const {withNx} = require('@nrwl/next/plugins/with-nx')
-
-function allowCamelCaseClassNames(config) {
-	const rules = config.module.rules
-		.find(rule => typeof rule.oneOf === 'object')
-		.oneOf.filter(rule => Array.isArray(rule.use))
-
-	rules.forEach(rule => {
-		rule.use.forEach(moduleLoader => {
-			const isCssLoader = moduleLoader.loader !== undefined
-				&& moduleLoader.loader.includes('css-loader')
-				&& typeof moduleLoader.options.modules === 'object'
-			if (isCssLoader) {
-				moduleLoader.options = {
-					...moduleLoader.options,
-					modules: {
-						...moduleLoader.options.modules,
-						exportLocalsConvention: 'camelCase',
-					},
-				}
-			}
-		})
-	})
-
-	return config
-}
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
 	nx: {
+		// Set this to true if you would like to to use SVGR
+		// See: https://github.com/gregberge/svgr
 		svgr: false,
 	},
-	webpack: allowCamelCaseClassNames,
+	webpack: config => {
+		const rules = config.module.rules
+			.find(rule => typeof rule.oneOf === 'object')
+			.oneOf.filter(rule => Array.isArray(rule.use))
+		rules.forEach(rule => {
+			rule.use.forEach(moduleLoader => {
+				if (
+					moduleLoader.loader !== undefined
+            && moduleLoader.loader.includes('css-loader')
+            && typeof moduleLoader.options.modules === 'object'
+				) {
+					moduleLoader.options = {
+						...moduleLoader.options,
+						modules: {
+							...moduleLoader.options.modules,
+							// This is where we allow camelCase class names
+							exportLocalsConvention: 'camelCase',
+						},
+					}
+				}
+			})
+		})
+
+		return config
+	},
 }
 
 module.exports = withNx(nextConfig)
